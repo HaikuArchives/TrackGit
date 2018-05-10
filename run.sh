a631fd6cec45f414d87e4276f335c0af8d7aef5b
Hrishi Hiraskar <hrishihiraskar@gmail.com>
2018-05-10 12:10:51 +0000

    Added Makefile

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-g++ TrackGit.cpp -o TrackGit -ltracker -lbe -I/boot/system/develop/headers/x86 -L/boot/system/develop/lib/x86 -lgit2
-cp TrackGit ~/config/non-packaged/add-ons/Tracker/
+make
+cp objects.x86-cc2-release/TrackGit ~/config/non-packaged/add-ons/Tracker/
