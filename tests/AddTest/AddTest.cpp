d27ac7b5fe5ef37dd2b74bf64de224a0cc264f99
Hrishi Hiraskar <hrishihiraskar@gmail.com>
2018-06-12 21:54:57 +0000

    Added Add file Tests

---
+++ tests/AddTest/AddTest.cpp
@@ -0,0 +1,65 @@
+#include "AddTest.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+#include <SupportKit.h>
+
+#include "../../GitCommand/Add.h"
+#include "../../GitCommand/Init.h"
+
+#define PATH "/boot/home/StatusTest"
+
+CPPUNIT_TEST_SUITE_REGISTRATION(AddTest);
+
+void
+AddTest::setUp(void)
+{
+	// Create a git repo at PATH
+	Init::InitRepo(PATH);
+
+	char command[1024];
+
+	// Add initial files
+	sprintf(command, "touch %s/a", PATH);
+	system(command);
+	sprintf(command, "touch %s/b", PATH);
+	system(command);
+
+	// Initialize status object
+	status = new Status(BString(PATH), BString(PATH));
+}
+
+void
+AddTest::tearDown(void) 
+{
+	char command[40];
+
+	// Remove created repo
+	sprintf(command, "rm -rf %s", PATH);
+	system(command);
+}
+
+void
+AddTest::test(void)
+{
+	// Add file a
+	vector<char*> files;
+	files.push_back("a");
+	Add::AddFiles(BString(PATH), files);
+
+	BString* statusText = status->GetStatusText();
+
+	// Check for new file
+	CPPUNIT_ASSERT (statusText->FindFirst("new file:  a") != B_ERROR);
+
+	// Add all file
+	files.clear();
+	Add::AddFiles(BString(PATH), files);
+
+	statusText = status->GetStatusText();
+
+	// Check for new file
+	CPPUNIT_ASSERT (statusText->FindFirst("new file:  b") != B_ERROR);
+}
