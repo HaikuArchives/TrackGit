cf1ae10f84877cbca813f18641ad289d209dd472
Andi Machovec <andi.machovec@gmail.com>
2018-10-23 15:00:31 +0000

    run.sh: enhanced script to work with gcc2 and 7 on both x86_gcc2 and x86_64

--- run.sh
+++ run.sh
@@ -1,2 +1,18 @@
+#!/bin/sh
+
+GCCVERSION=`gcc -dumpversion | cut -d'.' -f 1`
+ARCH=`uname -p`
+DEBUG=`cat Makefile | grep DEBUGGER |cut -d ' ' -f 3`
+
+OBJDIR=objects.$ARCH-cc$GCCVERSION
+
+if [ "$DEBUG" == "TRUE" ] 
+then
+	OBJDIR=$OBJDIR-debug
+fi
+
+echo "Compiling TrackGit ..."
 make
-cp objects.x86-cc2-debug/TrackGit ~/config/non-packaged/add-ons/Tracker/
+
+echo "Installing TrackGit add-on ..."
+cp -v $OBJDIR/TrackGit ~/config/non-packaged/add-ons/Tracker/	
